0c3def6b3496139156ca9660bb77257e7bf34527
Savithande HB <savithande1997@gmail.com>
2020-07-24 14:01:33 +0545

    first AngularJS

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var myApp = angular.module("myList", []);
+myApp.controller("myListController", function($scope){
+    $scope.items = ["Mango","Apple","Graps"];
+    $scope.newItem = "";
+   $scope.pushItem = function () {
+        if($scope.newItem != ""){
+            $scope.items.push($scope.newItem);
+            $scope.newItem = ""; 
+        }
+    }
+    $scope.deleteItem = function(index) {
+        $scope.items.splice(index, 1);
+    }
+
+});
